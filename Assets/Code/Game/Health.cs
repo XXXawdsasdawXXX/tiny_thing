a7b907b81887b35d3d10850d7290a041be003804
=) <xxx.awdsasdaw.xxx@gmail.com>
2025-03-02 17:41:28 +0300

    health

---
+++ Assets/Code/Game/Health.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using UnityEngine;
+
+namespace Code.Game
+{
+    public class Health : NetworkBehaviour
+    {
+        private readonly SyncVar<int> _health; 
+        public override void OnStartClient()
+        {
+            enabled = IsOwner;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                UpdateHealth(10);
+            }
+        }
+        
+        [ServerRpc]
+        public void UpdateHealth(int value)
+        {
+            _health.Value += value;
+            Debug.Log($"{gameObject.name} health {_health.Value}");
+        }
+    }
+}
