214fd84a90fb8285ee81519ac3b9e383b3a787d8
=) <xxx.awdsasdaw.xxx@gmail.com>
2025-03-02 10:18:49 +0300

    item spawner test

---
+++ Assets/Code/Game/HeroItemController.cs
@@ -0,0 +1,31 @@
+﻿using FishNet.Object;
+using UnityEngine;
+
+namespace Code.Game
+{
+    public class HeroItemController :  NetworkBehaviour
+    {
+        [SerializeField] private ItemSpawner _itemSpawner;
+        
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            
+            enabled = IsOwner;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                _itemSpawner.DropItemsRPC(transform.position + Vector3.right);
+            }
+
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                _itemSpawner.DespawnItemRPC();
+            }
+        }
+
+    }
+}
--- Assets/Code/Game/ItemSpawner.cs
+++ Assets/Code/Game/ItemSpawner.cs
@@ -1,4 +1,5 @@
-﻿using FishNet;
+﻿using System.Collections.Generic;
+using FishNet;
 using FishNet.Connection;
 using FishNet.Object;
 using Unity.VisualScripting;
@@ -9,51 +10,31 @@ namespace Code.Game
     public class ItemSpawner : NetworkBehaviour
     {
         [SerializeField] private Item _itemPrefab;
-        [SerializeField] private Item _itemInstance;
+        [SerializeField] private List<NetworkObject> _itemInstances;
         [SerializeField] private int _count;
-        public override void OnStartClient()
-        {
-            base.OnStartClient();
-            
-            enabled = IsOwner;
-        }
-
-        private void Update()
-        {
-            if (_itemInstance == null && Input.GetKeyDown(KeyCode.F1))
-            {
-               DropItemsRPC(_itemPrefab, transform.position + Vector3.right);
-            }
-
-            if (_itemInstance != null && Input.GetKeyDown(KeyCode.F2))
-            {
-                DeleteObject();
-                
-                _itemInstance = null;
-            }
-        }
-
-        [ServerRpc]
-        private void SpawnObject(GameObject instance)
-        {
-            
-            instance.SetActive(true);
-        }
-        
+      
         
         [ServerRpc]
-        private void DeleteObject()
-        {
-            ServerManager.Despawn(_itemInstance.gameObject);
-        }
-        
-        [ServerRpc]
-        private void DropItemsRPC(NetworkObject prefab, Vector3 position)
+        public void DropItemsRPC(Vector3 position)
         {
             var _networkManager = InstanceFinder.NetworkManager;
-            var drop = _networkManager.GetPooledInstantiated(prefab,position,Quaternion.identity,true);
+            var drop = _networkManager.GetPooledInstantiated(_itemPrefab, position,Quaternion.identity,true);
             _networkManager.ServerManager.Spawn(drop);
             _networkManager.SceneManager.AddOwnerToDefaultScene(drop);
+            _itemInstances.Add(drop);
+            // Отправляем клиенту ссылку на объект
+            //SetClientItemInstance(drop);
+        }
+
+
+        [ServerRpc]
+        public  void DespawnItemRPC()
+        {
+            var _networkManager = InstanceFinder.NetworkManager;
+    
+            _networkManager.ServerManager.Despawn(_itemInstances[^1]);
+
+            _itemInstances.Remove(_itemInstances[^1]);
         }
     }
 }
