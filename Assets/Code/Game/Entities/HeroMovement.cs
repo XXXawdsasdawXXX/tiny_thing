168cf7584a8bf785856abe47529bbfa817f7352b
=) <xxx.awdsasdaw.xxx@gmail.com>
2025-03-15 16:56:42 +0300

    Update HeroMovement.cs

--- Assets/Code/Game/Entities/HeroMovement.cs
+++ Assets/Code/Game/Entities/HeroMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace Game.Entities
 {
-    public class HeroMovement : NetworkBehaviour, IInitializeListener, IFixedUpdateListener
+    public class HeroMovement : NetworkBehaviour, IInitializeListener, IStartListener ,IFixedUpdateListener
     {
         [SerializeField] private Rigidbody2D _rigidbody2D;
         [SerializeField] private float _moveSpeed = 5f;
@@ -16,30 +16,27 @@ namespace Game.Entities
         private Camera _camera;
         private InputManager _inputManager;
         
-        public override void OnStartClient()
-        {
-            base.OnStartClient();
-
-            if (IsOwner)
-            {
-                _camera = Camera.main;
-
-                if (_camera != null)
-                {
-                    Transform cameraTransform = _camera.transform;
-                    Transform playerTransform = transform;
-
-                    Vector3 cameraPosition = playerTransform.position + new Vector3(0, _cameraYOffset, -10);
-
-                    cameraTransform.SetParent(playerTransform);
-                    cameraTransform.position = cameraPosition;
-                }
-            }
-        }
-
         public UniTask GameInitialize()
         {
             _inputManager = Container.Instance.GetService<InputManager>();
+            
+            return UniTask.CompletedTask;
+        }
+
+        public UniTask GameStart()
+        {
+            _camera = Camera.main;
+
+            if (_camera != null)
+            {
+                Transform cameraTransform = _camera.transform;
+                Transform playerTransform = transform;
+
+                Vector3 cameraPosition = playerTransform.position + new Vector3(0, _cameraYOffset, -10);
+
+                cameraTransform.SetParent(playerTransform);
+                cameraTransform.position = cameraPosition;
+            }
             
             return UniTask.CompletedTask;
         }
